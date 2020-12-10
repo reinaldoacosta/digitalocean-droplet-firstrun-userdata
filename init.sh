3a9d97df71a591be16c6a6fb1517127df7ecc8a5
Reinaldulin <12644557+reinaldoacosta@users.noreply.github.com>
2020-12-09 23:36:27 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,18 @@
 #update the system
-DEBIAN_FRONTEND=noninteractive apt update && apt upgrade -y
+RUN DEBIAN_FRONTEND=noninteractive apt update && apt upgrade -y
 
-#install docker from the snap store
-DEBIAN_FRONTEND=noninteractive apt install docker-compose docker.io -y
+#install dependencies
+RUN apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+#install docker from the docker offical repo
+RUN curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
+    apt-get install docker-ce
 
 #add alias commands
 echo "alias root='sudo -sE'" >> /root/.bashrc
