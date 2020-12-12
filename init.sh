8ecc607521078c5efd122a1a3b51a788ac919507
Reinaldulin <12644557+reinaldoacosta@users.noreply.github.com>
2020-12-12 19:13:21 -0400

    remove run

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 RUN DEBIAN_FRONTEND=noninteractive apt update && apt upgrade -y
 
 #install dependencies
-RUN apt-get install -y \
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -10,7 +10,7 @@ RUN apt-get install -y \
     software-properties-common
 
 #install docker from the docker offical repo
-RUN curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
     apt-get install docker-ce -y
 
